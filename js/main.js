27ca2bb4ea0ce77b6dddec3c6d524c98ce640640
Roger <rogerbolad@hotmail.com>
2024-05-08 14:57:31 -0300

    Otimizações e melhorias

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
-var backgroundVideo = document.getElementById("background-video");
-backgroundVideo.volume = 0.1;
-backgroundVideo.addEventListener("loadedmetadata", function () {
-	backgroundVideo.play();
+var video = document.getElementById("background-video");
+document.addEventListener("DOMContentLoaded", function () {
+	var playAudioButton = document.getElementById("play-audio-button");
+
+	video.volume = 0.25;
+
+	playAudioButton.addEventListener("click", function () {
+		video.muted = false;
+		playAudioButton.style.display = "none";
+	});
 });
 
+video.addEventListener("ended", function () {
+	document.body.style.backgroundImage = "url('../assets/img/bg.jpg')";
+	document.body.style.backgroundSize = "cover";
+	document.body.style.backgroundPosition = "center";
+	document.body.style.backgroundRepeat = "no-repeat";
+	video.parentNode.removeChild(video);
+	document.getElementById("linkedin-link").style.display = "block";
+	document.body.style.zIndex = "999";
+});
